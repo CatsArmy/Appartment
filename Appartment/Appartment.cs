38b1efc97575e84e31eb0181338fc6c8deecb29f
CatsArmy <bergman.itai@gmail.com>
2023-09-08 10:30:44 +0300

    Add project files.

---
+++ Appartment/Appartment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flats
+{
+    internal class Flat
+    {
+        int numFlat;
+        string ownerName;
+        int numRooms;
+        int familyMembers;
+        int[] payments;
+
+        public Flat(int numFlat, string ownerName, int numRooms, int familyMembers)
+        {
+            this.numFlat = numFlat;
+            this.ownerName = ownerName;
+            this.numRooms = numRooms;
+            this.familyMembers = familyMembers;
+            this.payments = new int[12];
+            for (int i = 0; i < this.payments.Length; i++)
+            {
+                this.payments[i] = 0;
+            }
+        }
+        public int GetNumFlat() => this.numFlat;
+        public void SetNumFlat(int flat) => this.numFlat = flat;
+        public string GetOwnerName() => this.ownerName;
+        public void SetOwnerName(string name) => this.ownerName = name;
+        public int CalculatePayment() => this.numRooms * 20 + this.familyMembers * 10;
+        /// <summary>
+        /// <paramref name="numMonth"/> is the index of the payment
+        /// </summary>
+        /// <param name="numMonth"></param>
+        public void Pay(int numMonth) => this.payments[numMonth - 1] = this.CalculatePayment();
+        public int Debt()
+        {
+            int count = 0;
+            foreach (int payment in this.payments)
+            {
+                if (payment == 0) { count++; }
+            }
+            return count * CalculatePayment();
+        }
+    }
+}
---
+++ Appartment/Program.cs
@@ -0,0 +1,7 @@
+﻿using Flats;
+
+Flat f1 = new Flat(1, "Lennon", 5, 4);
+Flat f2 = new Flat(2, "Star", 3, 2);
+f1.Pay(1);
+f2.Pay(2);
+Console.WriteLine($"Your debt is: {f2.Debt()}");
